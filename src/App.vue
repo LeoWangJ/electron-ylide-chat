7c1e7943fe53041f0708a3d983ded2d58b552c48
leowang <rfv7855659@gmail.com>
2022-11-09 16:13:47 +0800

    🛠 wip

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,56 @@ import HelloWorld from "./components/HelloWorld.vue";
 import fs from "fs";
 import { ipcRenderer } from "electron";
 import { onMounted } from "vue";
-onMounted(() => {
-  console.log(fs.writeFileSync);
-  console.log(ipcRenderer);
+import { everscaleBlockchainFactory, everscaleWalletFactory } from "@ylide/everscale";
+import { evmFactories, ethereumWalletFactory, EVMNetwork, EVM_NAMES } from "@ylide/ethereum";
+import { Ylide, IMessage, MessageContentV3, YlideKeyStore, BrowserLocalStorage, MessagesList, AbstractWalletController, AbstractBlockchainController, WalletControllerFactory, YlideKeyPair, GenericEntry, BlockchainSource, } from "@ylide/sdk";
+
+onMounted(async () => {
+  console.log(everscaleBlockchainFactory);
+
+  Ylide.registerBlockchainFactory(evmFactories[EVMNetwork.ETHEREUM]);
+  Ylide.registerBlockchainFactory(evmFactories[EVMNetwork.BNBCHAIN]);
+  Ylide.registerBlockchainFactory(evmFactories[EVMNetwork.POLYGON]);
+  Ylide.registerBlockchainFactory(evmFactories[EVMNetwork.ARBITRUM]);
+  Ylide.registerBlockchainFactory(evmFactories[EVMNetwork.OPTIMISM]);
+  Ylide.registerBlockchainFactory(evmFactories[EVMNetwork.AVALANCHE]);
+  Ylide.registerBlockchainFactory(everscaleBlockchainFactory);
+  Ylide.registerWalletFactory(ethereumWalletFactory);
+  Ylide.registerWalletFactory(everscaleWalletFactory);
+
+  // let YlideWallet: AbstractWalletController;
+
+  // const storage = new BrowserLocalStorage();
+  // console.log(storage)
+  // const keystore = new YlideKeyStore(storage, {
+  //   // This handler will be called every time Keystore
+  //   // needs user's Ylide password
+  //   onPasswordRequest: async (reason: string) =>
+  //     prompt(`Enter Ylide password for ${reason}:`),
+
+  //   // This handler will be called every time Keystore
+  //   // needs derived signature of user's Ylide password
+  //   onDeriveRequest: async (
+  //     reason: string, blockchainGroup: string,
+  //     wallet: string, address: string, magicString: string
+  //   ) => {
+  //     try {
+  //       // We request wallet to sign our magic string -
+  //       // it will be used for generation of communication
+  //       // private key
+  //       return YlideWallet.signMagicString(magicString);
+  //     } catch (err) {
+  //       return null;
+  //     }
+  //   },
+  // });
+
+  // await keystore.init();
+  // const ylide = new Ylide(keystore);
+
+  // let wallet = await ylide.addWallet('evm', 'web3');
+  // let reader = await ylide.addBlockchain('POLYGON');
+  // console.log(wallet, reader)
 });
 </script>
 
@@ -29,9 +76,11 @@ onMounted(() => {
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
